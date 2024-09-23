0a9223d9365e6b3371e54eb7fda7fa656b7b0a34
Jackson Kerr <jackk02468@gmail.com>
2024-09-23 14:35:11 -0500

    first outline

---
+++ Java/src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    
+    public class Note{
+        private String noteName;
+        private int octave;
+        public Note (String noteName, int octave) {
+            this.noteName = noteName;
+            this.octave = octave;
+        }
+    }
+    
+    
+    
+    private Note[] noteOptions(){
+        return null;
+    }
+
+    private int slideDistance(Note firstNote, Note secondNote){
+        //TODO
+        return 0;
+    }
+}
